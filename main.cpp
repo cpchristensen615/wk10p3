a744b8a8705ad7c1cd7b17a12f8ca96e3a2791a5
cpchristensen <112569319+cpchristensen615@users.noreply.github.com>
2022-10-30 22:13:34 +0000

    almost works except count is wrong

--- main.cpp
+++ main.cpp
@@ -2,25 +2,48 @@
 
 using namespace std;
 
+void cinArray(int array[], const int n);
+int findCount(int array[], int n, int count);
+void displaySeq(int array[], const int n);
+
 int main() {
   int N;
-  int biggest;
-  int next;
-  int count = 1;
   cin >> N;
-  cin >> biggest;  
-  for (int i = 0; i < (N-1); i++){
-      cin >> next;
-      if (next > biggest){
-        cout << biggest;
-        biggest = next;
-        count ++;
+  const int n = N;
+  int array[n];
+  int count = 0;
+
+  cinArray(array, n);
+  
+  cout <<  findCount(array, n, count) << endl;
+
+  displaySeq(array, n);
+  
+  
+}
+
+
+// user defined functions
+void cinArray(int array[], const int n){
+  for (int i = 0; i < n; i++)
+    cin >> array[i];
+}
+
+int findCount(int array[], const int n, int count){
+    for (int i = 0; i < n; i++){
+      if (array[i] > array[i+1])
+        count++;
       }
-      else
-        biggest = biggest;
-        
-    
+    return count;
+}
+
+void displaySeq(int array[], const int n){
+  int biggest = array[0];
+  cout << array[0] << " ";
+  for (int i = 0; i < n; i++){
+    if (biggest < array[i]){
+      cout << array[i] << " ";
+      biggest = array[i];
     }
-  cout << biggest << endl;
-  cout << count;
+  }
 }
